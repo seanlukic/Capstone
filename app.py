d778634357797c8be1534a7e401a3fe56d69538b
seanlukic <sean.lukic@gmail.com>
2026-01-30 02:01:36 -0500

    Rename page title and simplify button layout

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 import streamlit as st
 
-st.set_page_config(page_title="World Café Grouping Tool", layout="wide")
+st.set_page_config(page_title="Group Formation Tool", layout="wide")
 
 st.title("Multi-Step Optimization Model for Group Formation")
 st.caption("Generate diverse discussion groups based on participant traits")
 
-col1, col2, col3 = st.columns([1, 1, 1])
-with col2:
-    if st.button("Create Group Assignments", use_container_width=True):
-        st.switch_page("pages/1_Event_Setup.py")
+if st.button("Create Group Assignments"):
+    st.switch_page("pages/1_Event_Setup.py")
