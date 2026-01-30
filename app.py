52eb8446a857bb177543d57272e29e5bacabdbeb
seanlukic <sean.lukic@gmail.com>
2026-01-30 01:02:02 -0500

    Update title and add group assignment button

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 import streamlit as st
 
-st.title("World Café Grouping Tool")
-st.write("If you can see this, Streamlit is running.")
+st.set_page_config(page_title="World Café Grouping Tool", layout="wide")
+
+st.title("Multi-Step Optimization Model for Group Formation")
+st.caption("Generate diverse discussion groups based on participant traits")
+
+col1, col2, col3 = st.columns([1, 1, 1])
+with col2:
+    if st.button("Create Group Assignments", use_container_width=True):
+        st.switch_page("pages/1_Event_Setup.py")
