60279020076b524b4955ee558c6f8c925a8264ae
Sean Lukic <sean.lukic@gmail.com>
2026-02-01 19:47:29 -0500

    test updates

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(page_title="Group Formation Tool", layout="wide")
 
-st.title("Multi-Step Optimization Model for Group Formation")
+st.title("Multi-Step Optimization Model for Group Formation!")
 st.caption("Generate diverse discussion groups based on participant traits")
 
 if st.button("Create Group Assignments"):
